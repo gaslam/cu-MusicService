882432237c4eddd6aa9af774bae7086548fb6383
Dries Deboosere <dries.deboosere@howest.be>
2020-02-06 18:47:07 +0100

    Add Genre repo and controller

--- MusicService.API/Controllers/GenresController.cs
+++ MusicService.API/Controllers/GenresController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MusicService.API.Repositories;
+using MusicService.Domain.Models;
 using System.Threading.Tasks;
 
 namespace MusicService.API.Controllers
@@ -32,6 +33,58 @@ namespace MusicService.API.Controllers
             }
 
             return Ok(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostGenre([FromBody] Genre genre)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            await repository.Add(genre);
+
+            return CreatedAtAction("GetGenreById", new { id = genre.Id }, genre);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGenre([FromRoute] string id, [FromBody] Genre genre)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != genre.Id.ToString())
+            {
+                return BadRequest();
+            }
+
+            Genre updatedGenre = await repository.Update(genre);
+
+            if (updatedGenre == null)
+            {
+                return NotFound($"Genre was not found");
+            }
+
+            return Ok(updatedGenre);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGenre(string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var genre = await repository.Delete(id);
+            if (genre == null)
+            {
+                return NotFound($"Genre with id {id} was not found");
+            }
+
+            return Ok(genre);
         }
     }
 }
--- MusicService.API/Repositories/GenreRepository.cs
+++ MusicService.API/Repositories/GenreRepository.cs
@@ -3,6 +3,7 @@ using MusicService.API.Data;
 using MusicService.Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace MusicService.API.Repositories
@@ -25,6 +26,52 @@ namespace MusicService.API.Repositories
         public async Task<Genre> GetByIdAsync(string id)
         {
             return await db.Genres.FindAsync(Guid.Parse(id));
+        }
+
+        public async Task<Genre> Add(Genre genre)
+        {
+            db.Genres.Add(genre);
+            await db.SaveChangesAsync();
+            return genre;
+        }
+
+        public async Task<Genre> Update(Genre genre)
+        {
+            try
+            {
+                db.Entry(genre).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GenreExists(genre.Id))
+                {
+                    return null;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return genre;
+        }
+
+        public async Task<Genre> Delete(string id)
+        {
+            var genre = await GetByIdAsync(id);
+            if (genre == null)
+            {
+                return null;
+            }
+
+            db.Genres.Remove(genre);
+            await db.SaveChangesAsync();
+            return genre;
+        }
+
+        private bool GenreExists(Guid id)
+        {
+            return db.Genres.Any(g => g.Id == id);
         }
 
     }
