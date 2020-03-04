eaa9c0b5ee8e2a87ecdde05a64bf14fd79393b7c
Zarpor <ttt.g.com>
2020-03-04 08:41:59 +0100

    Add SaveImageInterface

---
+++ MusicService.API/Interfaces/IImageService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicService.API.Interfaces
+{
+    public interface IImageService
+    {
+        public Task<Uri> SaveImage(IFormFile formFile, string NewImageName);
+    }
+}
